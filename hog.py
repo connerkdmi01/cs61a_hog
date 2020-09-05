e42bf17686f89c4a803a9a3ad1e8a86d06d4a000
Ryan Zhu <36178918+alfphaderp@users.noreply.github.com>
2020-09-04 20:59:42 -0700

    Solve 7

--- hog.py
+++ hog.py
@@ -262,7 +262,20 @@ def announce_highest(who, last_score=0, running_high=0):
     """
     assert who == 0 or who == 1, 'The who argument should indicate a player.'
     # BEGIN PROBLEM 7
-    "*** YOUR CODE HERE ***"
+    def say(score0, score1):
+        if who == 0:
+            delta = score0 - last_score
+            if delta > running_high:
+                print(delta, "point(s)! The most yet for Player", who)
+                return announce_highest(who, score0, delta)
+            return announce_highest(who, score0, running_high)
+        else:
+            delta = score1 - last_score
+            if delta > running_high:
+                print(delta, "point(s)! The most yet for Player", who)
+                return announce_highest(who, score1, delta)
+            return announce_highest(who, score1, running_high)
+    return say
     # END PROBLEM 7
 
 
