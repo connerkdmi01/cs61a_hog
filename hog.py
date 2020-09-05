9efd5884d178d993fa0b6835ea05bac8214d6ebf
Ryan Zhu <36178918+alfphaderp@users.noreply.github.com>
2020-09-04 21:01:29 -0700

    More clean up

--- hog.py
+++ hog.py
@@ -171,11 +171,10 @@ def play(strategy0, strategy1, score0=0, score1=0, dice=six_sided,
             score1 += take_turn(roll1, score0, dice)
             if not extra_turn(score1, score0):
                 who = other(who)
-        say = say(score0, score1)
     # END PROBLEM 5
     # (note that the indentation for the problem 6 prompt (***YOUR CODE HERE***) might be misleading)
     # BEGIN PROBLEM 6
-    
+        say = say(score0, score1)
     # END PROBLEM 6
     return score0, score1
 
